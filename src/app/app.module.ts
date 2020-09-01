1a5afd2d7cfb08f5b860e7c64682fa5d4dadd2f1
hussainwali74 <13bscshwali@seecs.edu.pk>
2020-09-01 10:16:32 +0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,12 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 // import { LandingComponent } from './landing/landing.component';
 import { ProfileComponent } from './profile/profile.component';
-import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
 import { HomeModule } from './home/home.module';
 import { LoginComponent } from './login/login.component';
 import { AdminLayoutModule } from './layouts/admin-layout/admin-layout.module';
-import { CarModule } from './car/car.module';
 
 @NgModule({
   declarations: [
@@ -37,7 +35,6 @@ import { CarModule } from './car/car.module';
     RouterModule,
     AppRoutingModule,
     AdminLayoutModule,
-    CarModule,
     HomeModule,
   ],
   providers: [],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,20 +3,9 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { NotificationsComponent } from './pages/notifications/notifications.component';
-import { EventsComponent } from './pages/events/events.component';
-import { SearchComponent } from './pages/events/search/search.component';
-import { CalenderComponent } from './pages/events/calender/calender.component';
-import { CreatedComponent } from './pages/events/created/created.component';
-import { PromotionComponent } from './pages/events/promotion/promotion.component';
-import { CarHomeComponent } from './car/car-home/car-home.component';
-import { BuggatiComponent } from './car/buggati/buggati.component';
-import { SuzukiComponent } from './car/suzuki/suzuki.component';
+
 import { LandingComponent } from './pages/landing/landing.component';
 
 const routes: Routes = [
@@ -37,8 +26,8 @@ const routes: Routes = [
 
   { path: '', component: LandingComponent },
   // { path: 'profile', component: ProfileComponent },
-  { path: 'register', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: LoginComponent },
   // { path: 'landing', component: LandingComponent },
   // { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
